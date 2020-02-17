b3121a38603aecf6cc9f380749f4276f63c1b5a9
陈真 <luckytezuka@gmail.com>
2020-02-18 03:00:00 +0800

    登录校验

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 表单验证组件
+import './utils/validation.js'
+// postcss 插件，用于将单位转化为 rem
+import 'amfe-flexible'
+// 加载注册 Vant 组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'normalize.css'
+import './styles/base.less'
+import './styles/index.less'
+Vue.use(Vant)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
